9eb92dc767ba065cdc225d83ec995453c72938c6
alice <aliceisjustplaying@gmail.com>
2023-05-04 21:36:01 +0100

    getting there

--- src/app/alice/page.tsx
+++ src/app/alice/page.tsx
@@ -1,5 +1,10 @@
 import GA from '@/utils/ga';
 
+export const metadata = {
+  title: 'alice.bsky.sh',
+  description: "i'm alice",
+};
+
 export default function Home() {
   return (
     <>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import GA from '@/utils/ga';
 import Image from 'next/image';
 
+export const metadata = {
+  title: 'alice.bsky.sh',
+  description: "hi i'm alice",
+};
+
 export default function Home() {
   return (
     <>
