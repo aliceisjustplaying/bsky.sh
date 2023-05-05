07365ad9cadb29d163c75f2df48e8988221b9369
alice <aliceisjustplaying@gmail.com>
2023-05-05 11:24:51 +0100

    dark mode??????

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>{children}</body>
-      {/* <GA /> */}
+      <GA />
     </html>
   );
 }
