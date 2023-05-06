9facd99d2e9fb3ff60428143a5a83d8c2503d0a2
alice <aliceisjustplaying@gmail.com>
2023-05-06 16:27:46 +0100

    initial blog implementation

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { getAllPosts } from '@/utils/api';
+
+export default async function Page() {
+  const posts = await getAllPosts();
+
+  return (
+    <div>
+      <h1>My blog</h1>
+
+      <h2>All posts:</h2>
+      <ul>
+        {posts.map((post) => {
+          const { id, date, title } = post;
+          return (
+            <li key={id}>
+              <Link href={`/posts/${id}`}>
+                {date} - {title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
