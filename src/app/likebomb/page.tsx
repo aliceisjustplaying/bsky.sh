b1f290d090d4f83a3c6fc8f004a53b32ee12539f
alice <aliceisjustplaying@gmail.com>
2023-05-07 19:47:18 +0100

    credits, css

--- src/app/likebomb/page.tsx
+++ src/app/likebomb/page.tsx
@@ -7,13 +7,18 @@ export default function Page() {
   return (
     <>
       <div>
-        Drag this to your bookmarks bar. Clicking it will like every visible post in the thread.{' '}
+        Drag this to your bookmarks bar. Clicking it will like every visible post in the thread.
+        <br />
+        <br />
         <div
           dangerouslySetInnerHTML={{
             __html:
               '<a href="javascript:void%20function()%7Bconst%20a=document.querySelectorAll(%22div%5Baria-label=%5C%22Like%5C%22%5D%22),b=%5B...a%5D.filter(a=%3E!!a.offsetParent);b.forEach(a=%3Ea.click())%7D();">bsky like bomb</a>',
           }}
         ></div>
+        <br />
+        <a href="https://page.brick.do/like-bomb-bookmarklet-lkKreDvYyP4d">Original twitter version</a> by{' '}
+        <a href="https://staging.bsky.app/profile/did:plc:xbxpdv7nstw5ybmlneupnuaa">@nvpkp.bsky.social</a>.
       </div>
     </>
   );
