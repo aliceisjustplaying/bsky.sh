55c1113e7056020b635f80f1da6c967e1c3f7b5c
alice <aliceisjustplaying@gmail.com>
2023-05-08 21:04:19 +0100

    next update / fix test

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -1,45 +1,45 @@
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 
 interface person {
   name: string;
 }
 
-export function generateMetadata({ params }: { params: person }): Metadata {
-  const title = `${params.name}.bsky.sh`;
-  const description = 'nothing to see here';
-  const icon = '/nope.png';
-  const absoluteUrl = `https://${params.name}.bsky.sh`;
-  const absoluteIconUrl = `https://bsky.sh${icon}`;
+// export function generateMetadata({ params }: { params: person }): Metadata {
+//   const title = `${params.name}.bsky.sh`;
+//   const description = 'nothing to see here';
+//   const icon = '/nope.png';
+//   const absoluteUrl = `https://${params.name}.bsky.sh`;
+//   const absoluteIconUrl = `https://bsky.sh${icon}`;
 
-  return {
-    title: title,
-    description: description,
-    icons: {
-      icon: icon,
-      apple: icon,
-      shortcut: icon,
-    },
-    openGraph: {
-      title: title,
-      description: description,
-      url: absoluteUrl,
-      siteName: title,
-      images: [
-        {
-          url: absoluteIconUrl,
-        },
-      ],
-    },
-    twitter: {
-      card: 'summary',
-      site: absoluteUrl,
-      title: title,
-      description: description,
-      creator: '@alice.bsky.sh',
-      images: absoluteIconUrl,
-    },
-  };
-}
+//   return {
+//     title: title,
+//     description: description,
+//     icons: {
+//       icon: icon,
+//       apple: icon,
+//       shortcut: icon,
+//     },
+//     openGraph: {
+//       title: title,
+//       description: description,
+//       url: absoluteUrl,
+//       siteName: title,
+//       images: [
+//         {
+//           url: absoluteIconUrl,
+//         },
+//       ],
+//     },
+//     twitter: {
+//       card: 'summary',
+//       site: absoluteUrl,
+//       title: title,
+//       description: description,
+//       creator: '@alice.bsky.sh',
+//       images: absoluteIconUrl,
+//     },
+//   };
+// }
 
 export default function Page({ params }: { params: person }) {
   return (
