3a3a1c0a1cbb03217e082a78079d85570b73e6c9
alice <aliceisjustplaying@gmail.com>
2023-08-21 21:24:15 +0100

    link

--- src/app/alice/page.tsx
+++ src/app/alice/page.tsx
@@ -6,7 +6,9 @@ export const metadata = {
 export default function Page() {
   return (
     <>
-      <div>hi i&apos;m alice</div>
+      <div>
+        hi i&apos;m alice. <a href="https://bsky.app/profile/did:plc:by3jhwdqgbtrcc7q4tkkv3cf">find me on bluesky</a>
+      </div>
     </>
   );
 }
