d9fdb0490c2a26c17283df98a02a7edce0165ec0
alice <aliceisjustplaying@gmail.com>
2023-08-21 19:40:00 +0100

    a home page of sorts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'bsky.sh',
@@ -9,8 +10,13 @@ export default function Page() {
   return (
     <>
       <div className="main">
-        <Image src="/underconstruction.gif" width={459} height={51} alt="under construction" />
+        <Link href="https://likebomb.bsky.sh">💞 like bomb bookmarklet 💞</Link>
+        <Link href="https://showall.bsky.sh">🙈 show all bookmarklet 🙈</Link>
+        <Link href="https://alice.bsky.sh">me</Link>
       </div>
+      <footer>
+        <Image src="/underconstruction.gif" width={459} height={51} alt="under construction" />
+      </footer>
     </>
   );
 }
