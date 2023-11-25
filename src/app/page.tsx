465f6204ca032deb6ee0308b8704ec4fa54c8bb8
alice <aliceisjustplaying@gmail.com>
2023-11-25 19:29:25 +0000

    siiiiiiigh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import Image from 'next/image';
-import Link from 'next/link';
+// import Link from 'next/link';
+import { headers } from 'next/headers';
 
 export const metadata = {
   title: 'bsky.sh',
   description: 'bsky.sh',
 };
 
+const host = headers().get('host');
+const protocol = headers().get('x-forwarded-proto');
+
 export default function Page() {
   return (
     <>
       <div className="main">
-        <Link href="https://likebomb.bsky.sh">💞 like bomb bookmarklet 💞</Link>
+        {/* <Link href="https://likebomb.bsky.sh">💞 like bomb bookmarklet 💞</Link>
         <Link href="https://showall.bsky.sh">🙈 show all bookmarklet 🙈</Link>
-        <Link href="https://alice.bsky.sh">me</Link>
+        <Link href="https://alice.bsky.sh">me</Link> */}
+        <a href={`${protocol}://likebomb.${host}`}>💞 like bomb bookmarklet 💞</a>
+        <a href={`${protocol}://showall.${host}`}>🙈 show all bookmarklet 🙈</a>
+        <a href={`${protocol}://alice.${host}`}>me</a>
       </div>
       <footer>
         <Image src="/underconstruction.gif" width={459} height={51} alt="under construction" />
