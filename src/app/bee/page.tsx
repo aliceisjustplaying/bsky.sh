ccc6a1857e43912bf3d7d8737ee9b92a3e152ddb
alice <aliceisjustplaying@gmail.com>
2024-09-12 13:46:40 +0100

    bee

---
+++ src/app/bee/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: 'bee.bsky.sh',
+  description: "i'm bee",
+};
+
+export default function Page() {
+  return (
+    <>
+      <div>
+        hi i&apos;m bee. <a href="https://bsky.app/profile/did:plc:by3jhwdqgbtrcc7q4tkkv3cf">find me on bluesky</a>
+      </div>
+    </>
+  );
+}
