a886225de35cb9c981e0b46fd3e8f20ffdf33c85
alice <aliceisjustplaying@gmail.com>
2024-10-04 13:34:48 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,17 @@ export default async function Page() {
   return (
     <>
       <div className="main">
+        <h1>My Bluesky things</h1>
+        <Link href={`${protocol}://alice.${host}`}>@alice.bsky.sh</Link> (me)
+        <h2>Bookmarklets:</h2>
         <Link href={`${protocol}://likebomb.${host}`}>💞 like bomb bookmarklet 💞</Link>
         <Link href={`${protocol}://showall.${host}`}>🙈 show all bookmarklet 🙈</Link>
-        <Link href={`${protocol}://alice.${host}`}>me</Link>
-        {/* <Link href={`${protocol}://bee.${host}`}>bee</Link> */}
+        <h2>Bluesky Labelers:</h2>
         <Link href={`${protocol}://sortinghat.${host}`}>sorting hat bluesky labeler</Link>
         <Link href={`${protocol}://zodiacsigns.${host}`}>zodiac signs bluesky labeler</Link>
+        <h2>Other projects:</h2>
+        <Link href={`https://emojistats.bsky.sh`}>Emoji Stats for Bluesky</Link> <b>(New!)</b>
+        <Link href={`https://bluesky-heatmap.fly.dev`}>Bluesky Posts Heatmap Generator</Link>
       </div>
       <footer>
         <Image src="/underconstruction.gif" width={459} height={51} alt="under construction" />
