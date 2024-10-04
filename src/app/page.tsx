bd3926a6113cfcb9b5600c92a0b68a6af953b014
alice <aliceisjustplaying@gmail.com>
2024-10-04 13:41:44 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,20 +14,26 @@ export default async function Page() {
     <>
       <div className="main">
         <h1>My Bluesky things</h1>
-        <Link href={`${protocol}://alice.${host}`}>@alice.bsky.sh</Link> (me)
+        <Link href={`${protocol}://alice.${host}`}>@alice.bsky.sh</Link>&nbsp;(me)
         <h2>Bookmarklets:</h2>
         <Link href={`${protocol}://likebomb.${host}`}>💞 like bomb bookmarklet 💞</Link>
+        <br />
         <Link href={`${protocol}://showall.${host}`}>🙈 show all bookmarklet 🙈</Link>
         <h2>Bluesky Labelers:</h2>
         <Link href={`${protocol}://sortinghat.${host}`}>sorting hat bluesky labeler</Link>
+        <br />
         <Link href={`${protocol}://zodiacsigns.${host}`}>zodiac signs bluesky labeler</Link>
         <h2>Other projects:</h2>
-        <Link href={`https://emojistats.bsky.sh`}>Emoji Stats for Bluesky</Link> <b>(New!)</b>
+        <Link href={`https://emojistats.bsky.sh`}>
+          Emoji Stats for Bluesky
+        </Link>&nbsp;<b>(New!)</b>
+        <br />
+        
         <Link href={`https://bluesky-heatmap.fly.dev`}>Bluesky Posts Heatmap Generator</Link>
       </div>
-      <footer>
+      {/* <footer>
         <Image src="/underconstruction.gif" width={459} height={51} alt="under construction" />
-      </footer>
+      </footer> */}
     </>
   );
 }
